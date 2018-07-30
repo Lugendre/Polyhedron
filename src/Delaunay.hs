5df5b2b0d858f9974605b8d6fb9431f5b34bd276
Lugendre <franfranjk@gmail.com>
2018-07-30 20:59:52 +0900

    bug fix

--- src/Delaunay.hs
+++ src/Delaunay.hs
@@ -5,7 +5,7 @@ import Data.List
 
 -- Data Types
 
-data Circle = Circle R R deriving (Show)
+data Circle = Circle (Vector R) R deriving (Show)
 
 type Triangle = [Vector R]
 
@@ -19,11 +19,12 @@ hasCommonPoints :: Tetrahedron -> Tetrahedron -> Bool
 hasCommonPoints th1 th2 = sort th1 == sort th2
 
 getCircumcircle :: Tetrahedron -> Circle
-getCircumcircle (r1:th1) = luSolve (luPacked cm) dm
+getCircumcircle (r1:th1) = Circle cp rad
     where
-        cm = 2 * tr fromColumns (map (subtract r1) th1)
-        dm = tr fromColumns (map (subtract r2) th2)
+        cm = tr $ fromColumns (map (subtract r1) th1) * 2
+        dm = tr $ fromColumns (map (subtract r2) th2)
         r2 = cmap (^2) r1
-        th2 = cmap (^2) th2
-
+        th2 =  map (cmap (^2)) $ th1
+        cp = flatten $ luSolve (luPacked cm) dm
+        rad = norm_1 (cp - r1)
 
