5c72ca2e01d689753e74392b9e4d87c624d62562
Lugendre <franfranjk@gmail.com>
2018-07-30 13:32:37 +0900

    add Tetrahedral Volume & Area

---
+++ src/Polyhedron.hs
@@ -0,0 +1,25 @@
+module Polyhedron where
+
+import Numeric.LinearAlgebra
+
+-- Data Types
+
+type PolyHed = [Vector R]
+type Vectors = [Vector R]
+
+
+polyArea4 :: Vectors -> R
+polyArea4 (r1:r2:r3:_) = area r1 r2 + area r2 r3 + area r3 r1 + area r4 r5
+    where
+        r4 = r1 - r2
+        r5 = r2 - r3
+        area :: Vector R -> Vector R -> R
+        area ri rj = (norm_2  $ cross ri rj) / 2
+
+polyVol4 :: Vectors -> R
+polyVol4 = (/6) . det . fromColumns
+
+
+genVectors :: PolyHed -> Vectors
+genVectors [] = []
+genVectors (p1:ps) = map (subtract p1) ps
